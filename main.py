a79b61b555cd1c3f3f0c67e4e33db48b2adfb742
SmashyYT <jarnevdk@gmail.com>
2025-12-06 04:38:09 +0100

    Hoofdprogramma uitgebreid. Er is nu interactie met de gebruiker en een keuzemenu op het programma vlot te gebruiken.

--- main.py
+++ main.py
@@ -16,13 +16,58 @@ def display_books():
     if not boeken:
         print("Er zitten nog geen boeken in de collectie.")
     else:
-        print("\nTitel | Auteur")
-        print("-" * 30)
-        for boek in boeken:
-            print(f"{boek[1]} | {boek[2]}")
-            
-
+        #kolombreedtes voor mooie uitlijning
+        titel_width = max(len(boek[1]) for boek in boeken + [(0, "Titel", "Auteur")])
+        auteur_width = max(len(boek[2]) for boek in boeken + [(0, "Titel", "Auteur")])
         
+        print(f"\n{'Titel'.ljust(titel_width)} | {'Auteur'.ljust(auteur_width)}")
+        print("-" * (titel_width + auteur_width + 3))
+        
+        # data
+        for boek in boeken:
+            print(f"{boek[1].ljust(titel_width)} | {boek[2].ljust(auteur_width)}")
+            
+#auteurs tonen
+def display_authors():
+    auteurs = fetch_all_authors()
+    
+    if not auteurs:
+        print("Er zijn nog geen auteurs te vinden.")
+    else:
+        #kolombreedtes voor mooie uitlijning
+        naam_width = max(len(auteur[1]) for auteur in auteurs + [(0, "Naam")])
+        
+        print(f"\n{'Naam'.ljust(naam_width)}")
+        print("-" * naam_width)
+        
+        for auteur in auteurs:
+            print(f"{auteur[1].ljust(naam_width)}")
+            
+#boek toevoegen
+def add_book():
+    while True:
+        titel = input("Titel van het boek: ").strip()
+        
+        if(titel == ""): 
+            print("Titel mag niet leeg zijn, probeer opnieuw alstublieft.")
+        else:
+            break
+        
+    while True:
+        auteur_naam = input("Naam van de auteur: ").strip()
+        
+        if(auteur_naam == ""):
+            print("Naam van auteur mag niet leeg zijn, probeer opnieuw alstublieft.")
+        else:
+            break
+            
+    add_book_with_author(titel, auteur_naam)
+    print(f"Het boek '{titel}' van {auteur_naam} werd toegevoegd aan de boekencollectie!")
+    
+    
+    
+    
+    
     
 #hoofdprogramma    
 def main():
@@ -30,21 +75,17 @@ def main():
         show_menu()
         keuze = input("Maak een keuze: ").strip()
         
-        if keuze = "1":
+        if keuze == "1":
             display_books()
-        elif keuze = "2":
-            #voeg boek toe
-        elif keuze = "3":
-            #toon auteurs
-        elif keuze = "4":
+        elif keuze == "2":
+            add_book()
+        elif keuze == "3":
+            display_authors()
+        elif keuze == "4":
             print("Tot ziens!")
             break
         else:
             print("Ongeldige keuze, probeer opnieuw alstublieft.")
-
-
-
-
 
 
 if __name__ == "__main__":
